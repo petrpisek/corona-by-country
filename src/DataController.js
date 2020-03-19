ae9a752e1745125723f0bfa0ba727ec7459c3a56
PP <pisek.petr@gmail.com>
2020-03-19 15:53:11 +0100

    +active and deaths

--- src/DataController.js
+++ src/DataController.js
@@ -6,10 +6,45 @@ import format from "date-fns/format"
 
 const { getCode, getName, getData, overwrite } = require("country-list");
 
+overwrite([
+    { "code": "KR", "name": "S. Korea" },
+    { "code": "US", "name": "USA" },
+    { "code": "GB", "name": "UK" },
+    { "code": "AE", "name": "UAE" },
+    { "code": "TW", "name": "Taiwan" },
+    { "code": "FO", "name": "Faeroe Islands" },
+    { "code": "PS", "name": "Palestine" },
+    { "code": "VE", "name": "Venezuela" },
+    { "code": "CI", "name": "Ivory Coast" },
+    { "code": "CG", "name": "DRC" },
+    { "code": "BL", "name": "St. Barth" },
+    { "code": "MF", "name": "Saint Martin" },
+    { "code": "VI", "name": "U.S. Virgin Islands" },
+    { "code": "CF", "name": "CAR" },
+    { "code": "VA", "name": "Vatican City" },
+    { "code": "VC", "name": "St. Vincent Grenadines" },
+    { "code": "TZ", "name": "Tanzania" },
+
+    { "code": 'BN', "name": 'Brunei' },
+    { "code": "IR", "name": "Iran" },
+    { "code": "KR", "name": "Korea, South" },
+    { "code": "VN", "name": "Vietnam" },
+    { "code": "RU", "name": "Russia" },
+    { "code": "MD", "name": "Moldova" },
+    { "code": "BO", "name": "Bolivia" },
+    { "code": "GB", "name": "United Kingdom" },
+    { "code": "CG", "name": "Congo (Kinshasa)" },
+    { "code": "TW", "name": "Taiwan*" },
+]);
+
 export const DataController = (props) => {
 
     const [data, setData] = useState(null);
+    const [deathsData, setDeathsData] = useState(null);
+    const [activeData, setActiveData] = useState(null);
+    
     const [selected, setSelected] = useState([]);
+    const [graphType, setGraphType] = useState("total");
 
     function CSVtoArray(text) {
         var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/;
@@ -31,7 +66,7 @@ export const DataController = (props) => {
         return a;
     };
 
-    const parseWorldometersData = data => {
+    const parseWorldometersData = (data, dataType) => {
         var parser = new DOMParser();
         var htmlDoc = parser.parseFromString(data, 'text/html');
         var tableBody = htmlDoc.getElementById("main_table_countries_today").getElementsByTagName("tbody")[0];
@@ -41,7 +76,7 @@ export const DataController = (props) => {
         Array.from(rows).forEach(row => {
             const cells = row.cells;
             const country = cells[0].innerText.trim();
-            const cases = Number(cells[1].innerText.replace(",", ""));
+            const cases = Number(cells[dataType === "total" ? 1 : dataType === "active" ? 6 : 3].innerText.replace(",", ""));
 
             var code = getCode(country);
             if (code) {
@@ -54,148 +89,141 @@ export const DataController = (props) => {
         return result;
     }
 
-    useEffect(async () => {
-        async function f() {
-            const historicalData = fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
-                .then(res => res.text())
-                .then(res => res.split("\n").map(x => CSVtoArray(x)))
-                .catch(error => console.log('error', error));;
+    const loadData = async dataType => {
+        const historicalData = fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
+            .then(res => res.text())
+            .then(res => res.split("\n").map(x => CSVtoArray(x)))
+            .catch(error => console.log('error', error));;
 
-            const worldometersData = axios.get("https://cors-anywhere.herokuapp.com/https://www.worldometers.info/coronavirus")
-                .then(response => parseWorldometersData(response.data), error => console.log('error', error))
-                .catch(error => console.log('error', error));
+        const deathsData = fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv")
+            .then(res => res.text())
+            .then(res => res.split("\n").map(x => CSVtoArray(x)))
+            .catch(error => console.log('error', error));;
 
-            //.then(res => res.text())
-            //.then(res => parseWorldometersData(res))
-            //setData(d);
+        const recoveredData = fetch("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv")
+            .then(res => res.text())
+            .then(res => res.split("\n").map(x => CSVtoArray(x)))
+            .catch(error => console.log('error', error));;
 
-            overwrite([
-                { "code": "KR", "name": "S. Korea" },
-                {"code" : "US", "name": "USA"},
-                { "code": "GB", "name": "UK" },
-                { "code": "AE", "name": "UAE" },
-                { "code": "TW", "name": "Taiwan" },
-                { "code": "FO", "name": "Faeroe Islands" },
-                { "code": "PS", "name": "Palestine" },
-                { "code": "VE", "name": "Venezuela" },
-                { "code": "CI", "name": "Ivory Coast" },
-                { "code": "CG", "name": "DRC" },
-                { "code": "BL", "name": "St. Barth" },
-                { "code": "MF", "name": "Saint Martin" },
-                { "code": "VI", "name": "U.S. Virgin Islands" },
-                { "code": "CF", "name": "CAR" },
-                { "code": "VA", "name": "Vatican City" },
-                { "code": "VC", "name": "St. Vincent Grenadines" },
-                { "code": "TZ", "name": "Tanzania" },
+        const worldometersData = axios.get("https://cors-anywhere.herokuapp.com/https://www.worldometers.info/coronavirus")
+            .then(response => parseWorldometersData(response.data, dataType), error => console.log('error', error))
+            .catch(error => console.log('error', error));
 
-                { "code": 'BN', "name": 'Brunei' },
-                { "code": "IR", "name": "Iran" },
-                { "code": "KR", "name": "Korea, South" },
-                { "code": "VN", "name": "Vietnam" },
-                { "code": "RU", "name": "Russia" },
-                { "code": "MD", "name": "Moldova" },
-                { "code": "BO", "name": "Bolivia" },
-                { "code": "GB", "name": "United Kingdom" },
-                { "code": "CG", "name": "Congo (Kinshasa)" },
-                { "code": "TW", "name": "Taiwan*" },
-            ]);
+        //.then(res => res.text())
+        //.then(res => parseWorldometersData(res))
+        //setData(d);            
 
-            const d = await historicalData;
-            const currentData = await worldometersData;
+        const d = dataType === "total" ? await historicalData : await deathsData;
+        var allCases, recoveredCases;
+        if(dataType === "active") {
+            allCases = await historicalData;
+            recoveredCases = await recoveredData;
+        }
 
-            let dates = [];
+        const currentData = await worldometersData;
 
-            const countryData = {};
-            const countries = {};
-            let world = null;
-            let worldWithoutChina = null;
-            d.forEach((element, i) => {
-                if (!element)
-                    return;
+        let dates = [];
 
-                if (i === 0) {
-                    dates = element.slice(4);
-                    if (currentData && new Date(dates[dates.length - 1]).getDate() != new Date().getDate())
-                        dates.push(format(new Date(), "M/d/yy"));
-                    return;;
+        const countryData = {};
+        const countries = {};
+        let world = null;
+        let worldWithoutChina = null;
+        d.forEach((element, i) => {
+            if (!element)
+                return;
+
+            if (i === 0) {
+                dates = element.slice(4);
+                if (currentData && new Date(dates[dates.length - 1]).getDate() != new Date().getDate())
+                    dates.push(format(new Date(), "M/d/yy"));
+                return;;
+            }
+
+            var code = element[1].length === 2 ? element[1] : getCode(element[1]);
+
+            if (!code) {
+                console.log(`Code not found for ${element[1]}`);
+                return;
+            }
+
+            countryData[code] = (countryData[code] || 0) + dataType === "active" 
+            ? Number(allCases[i][element.length - 1]) - Number(element[element.length - 1]) - Number(recoveredCases[element.length - 1]) 
+            : Number(element[element.length - 1]);
+
+            var activeElement = dataType === "active" ? allCases[i].slice(4) : {};
+            var recoveredElement = dataType === "active" ? recoveredCases[i].slice(4) : {};
+            
+            var numbers = element.slice(4).map((x, i) => dataType === "active" ? Number(activeElement[i]) - Number(x) -  Number(recoveredElement[i]): Number(x) );
+
+            if (countries[code]) {
+                for (i = 0; i < countries[code].length; i++) {
+                    countries[code][i] += Number(numbers[i]);
                 }
+            } else {
+                countries[code] = [...numbers];
+            }
 
-                var code = element[1].length === 2 ? element[1] : getCode(element[1]);
-
-                if (!code) {
-                    console.log(`Code not found for ${element[1]}`);
-                    return;
+            if (!world) {
+                world = [...numbers];
+            }
+            else {
+                for (i = 0; i < world.length; i++) {
+                    world[i] += Number(numbers[i]);
                 }
+            }
 
-                countryData[code] = (countryData[code] || 0) + Number(element[element.length - 1]);
+            if (code === "CN") {
+                return;
+            }
 
-                var numbers = element.slice(4).map(x => Number(x));
+            if (!worldWithoutChina) {
+                worldWithoutChina = [...numbers];
+            }
+            else {
+                for (i = 0; i < worldWithoutChina.length; i++) {
+                    worldWithoutChina[i] += Number(numbers[i]);
+                }
+            }
+        });
 
-                if (countries[code]) {
-                    for (i = 0; i < countries[code].length; i++) {
-                        countries[code][i] += Number(numbers[i]);
-                    }
+        if (currentData) {
+            Object.keys(countries).forEach((code, i) => {
+                countryData[code] = currentData[code];
+                if (countries[code].length === dates.length) {
+                    if (currentData[code])
+                        countries[code][countries[code].length - 1] = currentData[code];
                 } else {
-                    countries[code] = [...numbers];
-                }
-
-                if (!world) {
-                    world = [...numbers];
-                }
-                else {
-                    for (i = 0; i < world.length; i++) {
-                        world[i] += Number(numbers[i]);
-                    }
-                }
-
-                if (code === "CN") {
-                    return;
-                }
-
-                if (!worldWithoutChina) {
-                    worldWithoutChina = [...numbers];
-                }
-                else {
-                    for (i = 0; i < worldWithoutChina.length; i++) {
-                        worldWithoutChina[i] += Number(numbers[i]);
+                    if (currentData[code]) {
+                        countries[code].push(currentData[code]);
+                    } else {
+                        countries[code].push(countries[code][countries[code].length - 1]);
                     }
                 }
             });
 
-            if (currentData) {
-                Object.keys(countries).forEach((code, i) => {
-                    countryData[code] = currentData[code];
-                    if (countries[code].length === dates.length) {
-                        if (currentData[code])
-                            countries[code][countries[code].length - 1] = currentData[code];
-                    } else {
-                        if (currentData[code]) {
-                            countries[code].push(currentData[code]);
-                        } else {
-                            countries[code].push(countries[code][countries[code].length - 1]);
-                        }
-                    }
-                });
+            var currentTotal = Object.values(currentData).reduce((a, b) => a + b);
 
-                var currentTotal = Object.values(currentData).reduce((a, b) => a+b);                
-
-                if (world.length === dates.length) {
-                    world[world.length -1] = currentTotal;
-                    worldWithoutChina[worldWithoutChina.length -1] = currentTotal - currentData["CN"];
-                } else {
-                    world.push(currentTotal);
-                    worldWithoutChina.push(currentTotal - currentData["CN"]);
-                }
-                
+            if (world.length === dates.length) {
+                world[world.length - 1] = currentTotal;
+                worldWithoutChina[worldWithoutChina.length - 1] = currentTotal - currentData["CN"];
+            } else {
+                world.push(currentTotal);
+                worldWithoutChina.push(currentTotal - currentData["CN"]);
             }
 
-            // Object.keys(countryData).sort().forEach(key => {
-            //     console.log(`${key}: ${countryData[key]}`)
-            // });
-
-            setData({ dates: dates, totalData: countryData, countries: countries, world: world, worldWithoutChina: worldWithoutChina })
         }
-        f();
+
+        // Object.keys(countryData).sort().forEach(key => {
+        //     console.log(`${key}: ${countryData[key]}`)
+        // });
+
+        return { dates: dates, totalData: countryData, countries: countries, world: world, worldWithoutChina: worldWithoutChina };
+    }
+
+    useEffect(() => {
+        loadData("total").then(x => setData(x));
+        loadData("deaths").then(x => setDeathsData(x));     
+        loadData("active").then(x => setActiveData(x));     
     }, [])
 
     const handleSelect = s => {
@@ -204,14 +232,25 @@ export const DataController = (props) => {
 
         setSelected(s);
     }
+
+    useEffect(() => {
+        loadData(graphType);
+    }, []);
+
     return (
         <div>
             <div>
                 <Map data={data} onSelect={handleSelect} />
-
+            </div>
+            <div style={{textAlign: "center"}}>
+                <label><input type="radio" name="total" value="total" checked={graphType === "total"} onClick={() => setGraphType("total")} /> Total</label>
+                <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                <label><input type="radio" name="deaths" value="deaths" checked={graphType === "deaths"} onClick={() => setGraphType("deaths")} /> Deaths</label>
+                <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                <label><input type="radio" name="active" value="active" checked={graphType === "active"} onClick={() => setGraphType("active")} /> Active</label>
             </div>
             <div>
-                <Chart data={data} selected={selected} />
+                <Chart data={graphType === "total" ? data : graphType === "active" ? activeData : deathsData} selected={selected} graphType={graphType} />
             </div>
         </div>
     )
